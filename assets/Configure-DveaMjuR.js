c131fc8d7d73aa6432a9adac592fd6608467ed43
Enos Domingues <mail@enos.dev>
2025-01-14 23:00:18 +0100

    Updates

---
+++ assets/Configure-DveaMjuR.js
@@ -0,0 +1,3 @@
+import{j as t}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as i}from"./index-BAvqEBP-.js";import{ae as r}from"./index-BONA85al.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DaDdB7B7.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";function o(n){const e={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Introduction"}),`
+`,t.jsx(e.h1,{id:"enosd---ui",children:"Enosd - UI"}),`
+`,t.jsx(e.p,{children:"This is a simple design system developed using React and the Shadcn UI component library."})]})}function l(n={}){const{wrapper:e}={...i(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{l as default};
